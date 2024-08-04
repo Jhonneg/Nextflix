f5895dadba5e3123a02805cbb49fb3a29a240bda
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-08-04 16:11:17 -0300

    added roboto font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Roboto_Slab } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto_Slab({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${styles.main} ${roboto.className}`}>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
