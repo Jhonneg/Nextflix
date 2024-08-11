5b90a89aa77f58f50357816a7eba277c3ed8c716
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-08-11 00:36:04 -0300

    banner styles complete

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,22 +1,32 @@
+import styles from "./navbar.module.css";
+
 export default function Navbar({ username }) {
   return (
-    <div>
-      <p>{username}</p>
-      <ul>
-        <li>Home</li>
-        <li>My List</li>
-      </ul>
-      <nav>
-        <div>
-          <button>
-            <p>{username}</p>
-          </button>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>Nextflix</div>
+        </a>
 
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
+        <nav className={styles.navContainer}>
           <div>
-            <a>Sign Out</a>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+            </button>
+
+            <div className={styles.navDropdown}>
+              <div>
+                <a className={styles.linkName}>Sign Out</a>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 }
