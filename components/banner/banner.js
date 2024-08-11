9da2c8eaadd6ebad79affa953e74bcfe57e95873
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-08-10 23:11:58 -0300

    initial banner component markup

---
+++ components/banner/banner.js
@@ -0,0 +1,22 @@
+export default function Banner({ title, subTitle, imgUrl }) {
+  function handleOnPlay() {
+    console.log("handleonplay");
+  }
+  return (
+    <div>
+      <h3>{title}</h3>
+      <h3>{subTitle}</h3>
+      <button onClick={handleOnPlay}>Play</button>
+      <div
+        style={{
+          backgroundImage: `url(${imgUrl})`,
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          backgroundSize: "cover",
+          backgroundPosition: "50% 50%",
+        }}
+      ></div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Roboto_Slab } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Banner from "@/components/banner/banner";
 
 const roboto = Roboto_Slab();
 
@@ -13,8 +14,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={`${styles.main} ${roboto.className}`}>
         <h1>Nextflix</h1>
+
+        <Banner
+          title="Bad Boys 2"
+          subTitle="best action movie ever"
+          imgUrl="/static/imageedit_1_8968412113.gif"
+        />
+
+        {/* <Navbar />
+        
+        <Card /> */}
       </main>
     </>
   );
